51092aeaf754c91c0318a27090caef7bdffeeeb1
LtMilligan <gioele.miscia@gmail.com>
2024-09-04 15:44:42 +0200

    [UPD] Adding html in dati.php and finish exercise

--- dati.php
+++ dati.php
@@ -3,8 +3,7 @@
     $paragrafo = $_GET['paragrafo'];
     $censura = $_GET['censura'];
 
-    echo $paragrafo;
-    echo $censura;
+    $paragrafoCensurato = str_replace($censura, '***', $paragrafo)
 
 ?>
 
@@ -16,6 +15,14 @@
     <title>Dati passati</title>
 </head>
 <body>
-    
+    <h1>Primo esercizio di PHP</h1>
+    <h3>Questo e' il paragrafo che e' stato scritto</h3>
+    <p><?php echo $paragrafo ?></p>
+    <h4>E questa e' la sua lunghezza</h4>
+    <p><?php echo strlen($paragrafo) ?></p>
+    <h3>Questo e' il paragrafo modificato</h3>
+    <p><?php echo $paragrafoCensurato ?></p>
+    <h4>E questa e' la sua lunghezza</h4>
+    <p><?php echo strlen($paragrafoCensurato) ?></p>
 </body>
 </html>
